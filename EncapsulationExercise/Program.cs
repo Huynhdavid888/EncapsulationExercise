ef68aceda8b0bc9d27972c1a6814d5300572342c
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-28 01:11:54 -0700

    initial init

---
+++ EncapsulationExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much money would you like to deposit?");
+            double amounToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amounToDeposit);
+            double userbalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userbalance,0:c}");
+
+        }
+
+    }
+}
+
